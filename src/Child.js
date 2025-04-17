9f23e460fdc5c2e613f3a6bb3a0c00243dda9190
Joseph Crump <josephcrump79@yahoo.com>
2025-04-16 19:55:20 -0500

    react-hooks-information-flow-code-along completed

--- src/Child.js
+++ src/Child.js
@@ -1,8 +1,22 @@
 import React from "react";
 import { getRandomColor } from "./randomColorGenerator.js";
 
-function Child({ onChangeColor }) {
-  return <div className="child" style={{ backgroundColor: "#FFF" }} />;
+function Child({ onChangeColor, color }) {
+  function handleClick() {
+    const newColor = getRandomColor();      // Child generates color
+    onChangeColor(newColor);                // Sends it to Parent
+  }
+
+  return (
+    <div
+      onClick={handleClick}
+      className="child"
+      style={{ backgroundColor: color }}
+    />
+  );
 }
+
+
+
 
 export default Child;
--- src/Parent.js
+++ src/Parent.js
@@ -3,13 +3,21 @@ import { getRandomColor } from "./randomColorGenerator.js";
 import Child from "./Child";
 
 function Parent() {
-  const randomColor = getRandomColor();
-  const [color, setColor] = useState(randomColor);
+  const [childrenColor, setChildrenColor] = useState("#FFF");
+  function handleChangeColor(newChildColor) {
+    const newRandomColor = getRandomColor();
+    setColor(newRandomColor);                // Update Parent background
+    setChildrenColor(newChildColor);         // Update both Child backgrounds
+  }
+  
+  
 
   return (
     <div className="parent" style={{ backgroundColor: color }}>
-      <Child />
-      <Child />
+      <Child color={childrenColor} onChangeColor={handleChangeColor} />
+      <Child color={childrenColor} onChangeColor={handleChangeColor} />
+
+
     </div>
   );
 }
